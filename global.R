dcaafcf3f7f2249c654d637060f176f5d8b2bf9e
jasmine <109494334+jasminex21@users.noreply.github.com>
2023-02-06 15:12:13 -0600

    Create global.R

---
+++ global.R
@@ -0,0 +1,21 @@
+### spotifySlug ###
+
+library(spotifyr)
+library(shiny)
+library(tidyverse)
+library(ggplot2)
+library(DT)
+library(shinyjs)
+library(RColorBrewer)
+library(wordcloud2)
+library(shinycustomloader)
+library(httr)
+
+# Setting up Spotify client ID and client secret
+auth = function(id, secret) {
+  Sys.setenv(SPOTIFY_CLIENT_ID = id)
+  Sys.setenv(SPOTIFY_CLIENT_SECRET = secret)
+  access_token = get_spotify_access_token()
+  options(httr_oauth_cache = T)
+  print("Successful authentication!")
+}
