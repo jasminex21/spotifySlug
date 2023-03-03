3167d423e38d996e196989e558764b027b4b73af
jasmine <109494334+jasminex21@users.noreply.github.com>
2023-03-03 12:51:56 -0600

    Update global.R

--- global.R
+++ global.R
@@ -18,6 +18,6 @@ auth = function(id, secret) {
   Sys.setenv(SPOTIFY_CLIENT_ID = id)
   Sys.setenv(SPOTIFY_CLIENT_SECRET = secret)
   access_token = get_spotify_access_token()
-  options(httr_oauth_cache = T)
+  # options(httr_oauth_cache = T)
   print("Successful authentication!")
 }
